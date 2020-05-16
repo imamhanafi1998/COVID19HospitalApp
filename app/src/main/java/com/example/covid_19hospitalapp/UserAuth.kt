2f0b9511ec532284e52f0d22f33db051ce1713c4
Imam Hanafi <h.imam1998@gmail.com>
2020-05-16 12:11:12 +0700

    added login and register features using sqlite

---
+++ app/src/main/java/com/example/covid_19hospitalapp/UserAuth.kt
@@ -0,0 +1,88 @@
+package com.example.covid_19hospitalapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.login.*
+import kotlinx.android.synthetic.main.user_container.*
+import kotlinx.android.synthetic.main.user_registration.*
+
+class UserAuth : AppCompatActivity() {
+
+    lateinit var handler: DatabaseHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.user_container)
+
+        handler = DatabaseHelper(this)
+
+        showHome()
+
+        save.setOnClickListener {
+            if (name.text.toString() == "" || password_register.text.toString() == "") {
+                Toast.makeText(this, "All fields are required", Toast.LENGTH_SHORT).show()
+            } else {
+                if (handler.insertUserData(name.text.toString(), password_register.text.toString())) {
+                    Toast.makeText(this, "Register Success", Toast.LENGTH_SHORT).show()
+                    name.text.clear()
+                    password_register.text.clear()
+                } else {
+                    Toast.makeText(this, "Username exists!!!", Toast.LENGTH_SHORT).show()
+                    password_register.text.clear()
+                }
+            }
+        }
+
+        login_button.setOnClickListener {
+            if (login_name.text.toString() == "" || login_password.text.toString() == "") {
+                Toast.makeText(this, "All fields are required", Toast.LENGTH_SHORT).show()
+            } else {
+                if (handler.userPresent(login_name.text.toString(), login_password.text.toString())) {
+                    Toast.makeText(this, "Login Success", Toast.LENGTH_SHORT).show()
+                    login_name.text.clear()
+                    login_password.text.clear()
+                    val intentMain = Intent(this, MainActivity::class.java)
+                    startActivity(intentMain)
+                } else {
+                    Toast.makeText(this, "Username or Password is incorrect", Toast.LENGTH_SHORT).show()
+                    login_password.text.clear()
+                }
+            }
+        }
+
+        fltr.setOnClickListener {
+            login_name.text.clear()
+            login_password.text.clear()
+            showRegistration()
+        }
+
+        frtl.setOnClickListener {
+            name.text.clear()
+            password_register.text.clear()
+            showLogin()
+        }
+    }
+
+
+
+    private fun showRegistration() {
+        registration_layout.visibility = View.VISIBLE
+        login_layout.visibility = View.GONE
+        user_c.visibility = View.GONE
+    }
+
+    private fun showLogin() {
+        registration_layout.visibility = View.GONE
+        login_layout.visibility = View.VISIBLE
+        user_c.visibility = View.GONE
+    }
+
+    private fun showHome() {
+        registration_layout.visibility = View.GONE
+        login_layout.visibility = View.VISIBLE
+        user_c.visibility = View.GONE
+    }
+}
